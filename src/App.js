eab859928bafd31608dbcdd4e2adab66b68b5d01
shivank <shivankkunwar100@gmail.com>
2023-01-19 13:54:42 +0530

    todo app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
+import { ProjectsProvider, SelectedProjectProvider } from './context';
+
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
+
+  return (
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <main
+          className={darkMode ? 'darkmode' : undefined}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Content />
+        </main>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  );
+};
+
+App.propTypes = {
+  darkModeDefault: PropTypes.bool,
+};
